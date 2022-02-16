588b74f1ad4142f231de60feb047dbc6c8fdef5a
Berkay Dereci <berkay_dereci@hotmail.com>
2022-02-17 00:47:09 +0300

    Comment lines edited.

--- database.js
+++ database.js
@@ -5,10 +5,11 @@ const DBSOURCE = "db.sqlite"
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
-      // Cannot open database
       console.error(err.message)
       throw err
-    }else{
+    }
+    else
+    {
         console.log('Connected to the SQLite database.')
         db.run(`CREATE TABLE user (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -18,10 +19,8 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
             CONSTRAINT email_unique UNIQUE (email)
             )`,
         (err) => {
-            if (err) {
-                // Table already created
-            }else{
-                // Table just created, creating some rows
+            if (err) {}
+            else{
                 var insert = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
                 db.run(insert, ["admin","admin@example.com",md5("admin123456")])
                 db.run(insert, ["user","user@example.com",md5("user123456")])
@@ -29,6 +28,5 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
         });  
     }
 });
-
 
 module.exports = db
